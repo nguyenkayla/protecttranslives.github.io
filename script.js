abc8b08fd4c9466d03521fb8971dc1a84af71a0b
nguyenkayla <69479689+nguyenkayla@users.noreply.github.com>
2021-10-13 19:18:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,36 @@ function displayStat() {
     count = 0;
   }
 }
+
+function showTime(){
+    var date = new Date();
+    var d = date.getDate();
+    var d1 = date.getDay();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "PM";
+    
+    if(h == 02){
+        h = 12;
+    }
+    
+    
+    if(h > 12){
+        session = "PM";
+    }
+    if(h < 12) {
+    session = "AM"
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = d + "/"+  d1+ "  " +  "Time:" + "  " + h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    }
+showTime();
